d6ce1de1b9319f88a85041197ef7c069ef3fabc1
simonloeser <92864888+simonloeser@users.noreply.github.com>
2023-12-01 10:22:44 +0100

    feat: init Day1

--- src/Main.java
+++ src/Main.java
@@ -7,16 +7,16 @@ public class Main {
         System.out.println("\n  #### AoC 2023 ####\n----------------------\n");
         System.out.print("Daynumber to start: ");
 
-        Map<Integer, Runnable> days = new HashMap<>();
+        Map<Integer, Runnable> dayMap = new HashMap<>();
 
         // Solutions
-        // days.put(1, DayOne::solve);
+        dayMap.put(1, Day1::solve);
 
         Scanner reader = new Scanner(System.in);
         int input = reader.nextInt();
         reader.close();
         System.out.println("\n----------------------\n");
 
-        days.get(input).run();
+        dayMap.get(input).run();
     }
 }
---
+++ src/days/Day1.java
@@ -0,0 +1,6 @@
+public class Day1 {
+    public static void solve() {
+        String[] inputDay1 = ReadInput.getText("Day1");
+        System.out.println(inputDay1);
+    }
+}
